faa73ea19c942bcc3c7816e76cbb313755619d88
abhijeeth07 <95340269+abhijeeth07@users.noreply.github.com>
2021-12-03 14:05:53 +0530

    Add files via upload

---
+++ smallest number among 3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	printf("enter the numbers");
+	scanf("%d",&a);
+	
+printf("enter the number");
+scanf("%d",&b);
+
+printf("enter the number");
+scanf("%d",&c);
+
+if (a<b && a<c)
+printf("a is smallest value" );
+if (c<b && c<a)
+printf("c is the smallest");
+
+
+}
